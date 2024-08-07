7c7cdd396a892e857ffb2c8e29d684966c48dbf5
Anh Ly <lylyanh135@gmail.com>
2024-08-07 14:02:56 +0700

    feat: add migration nullable last modified time

--- backend/Domain/Core/AuditableEntity.cs
+++ backend/Domain/Core/AuditableEntity.cs
@@ -4,6 +4,6 @@ public class AuditableEntity : Entity<Guid>
 {
     public DateTime CreatedTime { get; set; }
     public string CreatedBy { get; set; }
-    public DateTime LastModifiedTime { get; set; }
+    public DateTime? LastModifiedTime { get; set; }
     public string LastModifiedBy { get; set; }
 }
